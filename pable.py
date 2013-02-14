e29c201b2cc74a25f1246f366e12cd10af014580
Bernardo B. Marques <bernardo.fire@gmail.com>
2013-02-14 17:50:04 -0200

    create Row and Cell

---
+++ pable.py
@@ -0,0 +1,22 @@
+class Row(object):
+
+    def __init__(self, array=[]):
+        self.cell_index = 0
+        self.cells = []
+        for item in array:
+            self.add_cell(item)
+
+    def add_cell(self, item):
+        options = { 'value': item, 'index': self.cell_index }
+        self.cells.append(Cell(options))
+        self.cell_index += 1
+
+
+class Cell(object):
+
+    def __init__(self, options):
+        self.value = options['value']
+        self.index = options['index']
+
+    def lines(self):
+        len(self.value.split('\n'))
