d6bcd3028f5ec2a659e9c231ea65e19495b3c386
Bernardo B. Marques <bernardo.fire@gmail.com>
2013-02-15 22:53:25 -0200

    a looooooot of code

--- pable.py
+++ pable.py
@@ -3,22 +3,52 @@ class Table(object):
     def __init__(self, options):
         self.add_rows(options['rows'])
         self.column_widths = []
+        self.style = Style()
 
     def add_rows(self, array):
         self.rows = []
         for r in array:
-            self.rows.append(Row(r))
+            self.rows.append(Row(table=self, array=r))
+
+    def render(self):
+        separator = Separator(table=self)
+        buffer = self.rows
+        buffer.insert(0, separator)
+        buffer.append(separator)
+        import pdb; pdb.set_trace()
+
+    def cell_spacing(self):
+        self.cell_padding() + len(self.style.border_y)
+
+    def cell_padding(self):
+        self.style.padding_left + self.style.padding_right
+
+    def column_width(self, n):
+        try: n = self.column_widths[n]
+        except: n = 0
+        return n
+
+    def columns_width(self):
+        last = 0
+        for i in self.columns_widths: last += (i + len(self.style.border_y))
+
+    def columns_number(self):
+        return max(len(c.cells) for c in self.headings_with_rows())
+
+    def headings_with_rows(self):
+        return self.rows #TODO: headings
+
 
 class Row(object):
 
-    def __init__(self, array=[]):
+    def __init__(self, table, array=[]):
+        self.table = table
         self.cell_index = 0
         self.cells = []
-        for item in array:
-            self.add_cell(item)
+        for item in array: self.add_cell(item)
 
     def add_cell(self, item):
-        options = { 'value': item, 'index': self.cell_index }
+        options = { 'value': item, 'index': self.cell_index, 'table': self.table }
         self.cells.append(Cell(options))
         self.cell_index += 1
 
@@ -31,22 +61,30 @@ class Cell(object):
     def __init__(self, options):
         self.value = options['value']
         self.index = options['index']
+        self.table = options['table']
 
     def lines(self):
         return self.value.split('\n')
 
 
 class Separator(Row):
-    def render():
-        pass
+    def render(self):
+        arr_x = []
+        for i in range(self.table.columns_number()):
+            arr_x.append(self.table.style.border_x * (self.table.column_width(i)
+                + self.table.cell_padding()))
+        border_i = self.table.style.border_i
+        return border_i + border_i.join(arr_x) + border_i
 
 
 class Style(object):
 
     def __init__(self):
-        self.defaults = {
-            'border_x': '-', 'border_y': '|',
-            'border_i': '+', 'padding_left': 1,
-            'padding_right': 1, 'width': None,
-            'alignment': None
-        }
+        self.border_x = '-'
+        self.border_y = '|'
+        self.border_i = '+'
+        self.padding_left = 1
+        self.padding_right = 1
+        self.width = None,
+        self.alignment = None
+
