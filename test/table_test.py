3c0a2676230581c68e72092c4f21d81658673bee
Bernardo B. Marques <bernardo.fire@gmail.com>
2013-02-21 16:09:12 -0300

    test Row#add_cell

--- test/table_test.py
+++ test/table_test.py
@@ -6,10 +6,20 @@ class RowTest(unittest.TestCase):
 
     def test_create(self):
         table = Table([['a']])
-        row = Row(table, table.rows_values[0])
+        row = table.rows[0]
         self.assertEqual(row.table, table)
         self.assertEqual(row.cell_index, 1)
         self.assertEqual(row.cells_values, ['a'])
+
+    def test_add_cell(self):
+        row = Table([['a']]).rows[0]
+        row.add_cell('foo')
+        cell = row.cells[0]
+        self.assertEqual(row.cell_index, 2)
+        self.assertEqual(len(row.cells), 1)
+        self.assertEqual(cell.value, 'foo')
+        self.assertEqual(cell.index, 1)
+        self.assertEqual(cell.table, row.table)
 
 class CellTest(unittest.TestCase):
 
